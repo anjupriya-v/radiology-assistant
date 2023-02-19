a9c40b5646ded6d81a8ae47bbfd232f15dfdaef5
anjupriya-v <anjupriyachennai@gmail.com>
2023-02-20 02:33:29 +0530

    Added email.js

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,18 +1,21 @@
+(function () {
+  emailjs.init("PublicKey_XXXXXXXXXXXXXX");
+})();
 document
   .getElementsByClassName("submit-contact-form")[0]
   .addEventListener("submit", (e) => {
-    var name = document.forms.form.name;
-    var email = document.forms.form.email.value;
-    var textArea = document.forms.form.textArea.value;
+    e.preventDefault();
+    var name = document.getElementsByClassName("name")[0].value;
+    var email = document.getElementsByClassName("email")[0].value;
+    var message = document.getElementsByClassName("message")[0].value;
     var nameError = document.getElementsByClassName("nameError")[0];
     var emailError = document.getElementsByClassName("emailError")[0];
-    var textAreaError = document.getElementsByClassName("textAreaError")[0];
+    var messageError = document.getElementsByClassName("messageError")[0];
 
     var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g; //Javascript reGex for Email Validation.
     var regName = /\d+$/g; // Javascript reGex for Name validation
 
     if (name == "" || regName.test(name)) {
-      e.preventDefault();
       nameError.style.display = "block";
       nameError.innerHTML = "Please enter your name properly.";
       return false;
@@ -20,8 +23,6 @@ document
       nameError.style.display = "none";
     }
     if (email == "" || !regEmail.test(email)) {
-      e.preventDefault();
-
       emailError.style.display = "block";
       emailError.innerHTML = "Please enter a valid e-mail address.";
 
@@ -30,15 +31,32 @@ document
       emailError.style.display = "none";
     }
 
-    if (textArea == "") {
-      e.preventDefault();
-
-      textAreaError.style.display = "block";
-      textAreaError.innerHTML = "Please enter your password";
+    if (message == "") {
+      messageError.style.display = "block";
+      messageError.innerHTML = "Please enter your message";
 
       return false;
     } else {
-      textAreaError.style.display = "none";
+      messageError.style.display = "none";
     }
+
+    var contactParams = {
+      fullname: name,
+      emailid: email,
+      message: message,
+    };
+    emailjs
+      .send("service_XXXXXX", "template_XXXXXXX", contactParams)
+      .then(function (res) {
+        document.getElementsByClassName(
+          "contact-success-message"
+        )[0].style.display = "block";
+        setTimeout(() => {
+          document.getElementsByClassName(
+            "contact-success-message"
+          )[0].style.display = "none";
+        }, 5000);
+        document.getElementsByClassName("contact-form")[0].reset();
+      });
     return true;
   });
