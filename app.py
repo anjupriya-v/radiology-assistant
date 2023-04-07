26e56ba9f7eb71d1dc14d2c3eb43ddba88e64edb
anjupriya-v <anjupriyachennai@gmail.com>
2023-04-07 10:20:43 +0530

    Made some changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from functools import wraps
 from flask import Flask,render_template,request,redirect,session,url_for
 import operator
 import cv2
-from flask_cors import CORS # opencv library
+from flask_cors import CORS
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
@@ -39,7 +39,6 @@ CORS(app)
 model=load_model('gesture.h5')
 print("Loaded model from disk")
 
-
 @app.route('/')# route to display the home page
 def home():
     if(session):
@@ -64,7 +63,6 @@ def start_session(userInfo):
     session['user']=userInfo
     return redirect(url_for('home'))
 
-
 @app.route('/login/',methods=['POST','GET'])
 def login():
     if request.method=="GET":
@@ -83,8 +81,7 @@ def login():
         return render_template('login.html',loginMessage='Sorry, user with this email id does not exist',show=True)
     else:
         return render_template('login.html',loginMessage='Sign In Failed!',show=True)
-
-
+    
 @app.route('/signup/',methods=['POST','GET'])
 def signup():
     if request.method=="GET":
@@ -104,25 +101,21 @@ def signup():
             return render_template('login.html',signupSuccess='Your account has been registered! Please log in',show=True)
     else:
         return render_template("signup.html",signupMessage='signup failed',show=True)
-
-
+    
 @app.route('/logout/',methods=["GET"])
 def logout():
     if request.method=="GET":
         session.clear()
     return redirect(url_for('login'))
 
-
 @app.route('/launch/',methods=['GET','POST'])# routes to the index html
 @login_required
 def launch():
     return render_template("launch.html",data=session['user'],loggedIn=session['logged_in'],show=False,handpositions=handPositionsData['handPositions'])
 
-
 @app.route('/predict/',methods=['GET', 'POST'])# route to show the predictions in a web UI
 @login_required
 def predict():
-
     if request.method == 'POST':
         f = request.files['image']
         basepath = os.path.dirname(__file__)
@@ -134,7 +127,6 @@ def predict():
             _, frame = cap.read() #capturing the video frame values
             # Simulating mirror image
             frame = cv2.flip(frame, 1)
-            
             # Got this from collect-data.py
             # Coordinates of the ROI
             x1 = int(0.5*frame.shape[1]) 
@@ -146,7 +138,6 @@ def predict():
             cv2.rectangle(frame, (x1-1, y1-1), (x2+1, y2+1), (255,0,0) ,1)
             # Extracting the ROI
             roi = frame[y1:y2, x1:x2]
-            
             # Resizing the ROI so it can be fed to the model for prediction
             roi = cv2.resize(roi, (64, 64)) 
             roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
@@ -162,31 +153,24 @@ def predict():
                           'FIVE': result[0][5]}
             # Sorting based on top prediction
             prediction = sorted(prediction.items(), key=operator.itemgetter(1), reverse=True)
-            
             # Displaying the predictions
             cv2.putText(frame, prediction[0][0], (10, 120), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)    
             cv2.imshow("Frame", frame)
-            
             #loading an image
             image1=cv2.imread(file_path)
             if prediction[0][0]=='ONE':
-               
                 resized = cv2.resize(image1, (200, 200))
                 cv2.imshow("Fixed Resizing", resized)
                 key=cv2.waitKey(3000)
-                
                 if (key & 0xFF) == ord("1"):
                     cv2.destroyWindow("Fixed Resizing")
-            
             elif prediction[0][0]=='ZERO':
-                
                 cv2.rectangle(image1, (480, 170), (650, 420), (0, 0, 255), 2)
                 cv2.imshow("Rectangle", image1)
                 cv2.waitKey(0)
                 key=cv2.waitKey(3000)
                 if (key & 0xFF) == ord("0"):
                     cv2.destroyWindow("Rectangle")
-                
             elif prediction[0][0]=='TWO':
                 (h, w, d) = image1.shape
                 center = (w // 2, h // 2)
@@ -196,22 +180,18 @@ def predict():
                 key=cv2.waitKey(3000)
                 if (key & 0xFF) == ord("2"):
                     cv2.destroyWindow("OpenCV Rotation")
-                
             elif prediction[0][0]=='THREE':
                 blurred = cv2.GaussianBlur(image1, (21, 21), 0)
                 cv2.imshow("Blurred", blurred)
                 key=cv2.waitKey(3000)
                 if (key & 0xFF) == ord("3"):
                     cv2.destroyWindow("Blurred")
-
             elif prediction[0][0]=='FOUR':
-               
                 resized = cv2.resize(image1, (400, 400))
                 cv2.imshow("Fixed Resizing", resized)
                 key=cv2.waitKey(3000)
                 if (key & 0xFF) == ord("4"):
                     cv2.destroyWindow("Fixed Resizing")
-
             elif prediction[0][0]=='FIVE':
                 '''(h, w, d) = image1.shape
                 center = (w // 2, h // 2)
@@ -222,16 +202,11 @@ def predict():
                 key=cv2.waitKey(3000)
                 if (key & 0xFF) == ord("5"):
                     cv2.destroyWindow("OpenCV Gray Scale")
-
             else:
                 continue
-            
-            
             interrupt = cv2.waitKey(10)
             if interrupt & 0xFF == 27: # esc key
                 break
-                
-         
         cap.release()
         cv2.destroyAllWindows()
     return render_template("launch.html")
